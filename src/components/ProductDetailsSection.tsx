d9c8741b26ec6deee3b78d5b49b3a02e4a3ccba6
rishu <rishabg7269@gmail.com>
2024-09-23 22:29:23 +0530

    beautify product specifications in product details page.

--- src/components/ProductDetailsSection.tsx
+++ src/components/ProductDetailsSection.tsx
@@ -8,11 +8,16 @@ interface ProductDetailsSectionProps {
 const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = ({ product }) => (
     <div className={styles.productDetails}>
         <h4>Product Details:</h4>
-        <p><strong>Material & Care:</strong> {product.material}</p>
-        <p><strong>Care Instructions:</strong> {product.careInstructions}</p>
+        <p><strong>Material & Care:</strong></p>
+        <p>{product.material}</p>
+        <p>{product.careInstructions}</p>
+
         <p><strong>Country of Origin:</strong> {product.countryOfOrigin}</p>
-        <p><strong>Manufactured & Sold By:</strong> {product.manufacturer}</p>
+
+        <p><strong>Manufactured & Sold By:</strong></p>
+        <p>{product.manufacturer}</p>
         <p>{product.manufacturerAddress}</p>
+
         <p><strong>Customer Care No.:</strong> {product.customerCareNumber}</p>
         <p><strong>Email:</strong> <a href={`mailto:${product.email}`}>{product.email}</a></p>
     </div>
