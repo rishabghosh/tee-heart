4f4d4290797fddd30519603b500d2f9cdb7f88cd
rishu <rishabg7269@gmail.com>
2024-11-20 09:40:29 +0530

    enhance details of product details page

--- src/components/ProductDetailsSection.tsx
+++ src/components/ProductDetailsSection.tsx
@@ -1,33 +1,48 @@
 import styles from '@/styles/ProductDetails.module.scss';
 import {ProductExtended} from '@/models/ProductExtended';
+import {useState} from "react";
 
 interface ProductDetailsSectionProps {
     product: ProductExtended;
 }
 
-const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = ({product}) => (
-    <div className={styles.productDetails}>
-        <h3>Product Details:</h3>
+const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = ({product}) => {
+    const [isOpen, setIsOpen] = useState(true);
 
-        <div className={styles.section}>
-            <strong>Material & Care:</strong>
-            <p>{product.material}</p>
-            <p>{product.careInstructions}</p>
+    const toggleContent = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={styles.productDetails}>
+            <div className={styles.header} onClick={toggleContent}>
+                <h3>Product Details</h3>
+                <span className={styles["toggle-icon"]}>{isOpen ? '▲' : '▼'}</span>
+            </div>
+
+            <div className={`${styles.content} ${isOpen ? styles.show : ""}`}>
+
+                <div className={styles.section}>
+                    <strong>Material & Care:</strong>
+                    <p>{product.material}</p>
+                    <p>{product.careInstructions}</p>
+                </div>
+
+                <div className={styles.section}>
+                    <strong>Country of Origin:</strong> {product.countryOfOrigin}
+                </div>
+
+                <div className={styles.section}>
+                    <strong>Manufactured & Sold By:</strong>
+                    <p>{product.manufacturer}</p>
+                    <p>{product.manufacturerAddress}</p>
+                    <p><strong>Customer Care No.:</strong> {product.customerCareNumber}</p>
+                    <p><strong>Email:</strong> <a href={`mailto:${product.email}`}>{product.email}</a></p>
+                </div>
+            </div>
+
         </div>
-
-        <div className={styles.section}>
-            <strong>Country of Origin:</strong> {product.countryOfOrigin}
-        </div>
-
-        <div className={styles.section}>
-            <strong>Manufactured & Sold By:</strong>
-            <p>{product.manufacturer}</p>
-            <p>{product.manufacturerAddress}</p>
-            <p><strong>Customer Care No.:</strong> {product.customerCareNumber}</p>
-            <p><strong>Email:</strong> <a href={`mailto:${product.email}`}>{product.email}</a></p>
-        </div>
-
-    </div>
-);
+    );
+};
 
 export default ProductDetailsSection;
