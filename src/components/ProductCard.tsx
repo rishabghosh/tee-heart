2af584c4150cbc0490251e0d52ccd26086814c73
rishu <rishabg7269@gmail.com>
2025-01-03 16:57:42 +0530

    improve product card design in browsing page for laptop resolutions.
    it breaks little bit mobile resolutions.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/ProductCard.module.scss';
-import {addDomain} from "@/utils/envUrls";
-import {ProductExtended} from "@/models/ProductExtended";
+import { addDomain } from "@/utils/envUrls";
+import { ProductExtended } from "@/models/ProductExtended";
 
 interface ProductCardProps {
     product: ProductExtended;
@@ -11,17 +11,23 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return (
         <div className={styles.card}>
-            <Link href={`/product/${product.id}`}>
-                <Image
-                    src={addDomain(product.imageUrls[0])}
-                    alt={product.name}
-                    className={styles.image}
-                    width={500}
-                    height={500}
-                />
-                <h2 className={styles.name}>{product.name}</h2>
+            <Link href={`/product/${product.id}`} className={styles.link}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src={addDomain(product.imageUrls[0])}
+                        alt={product.name}
+                        className={styles.image}
+                        width={500}
+                        height={500}
+                    />
+                    {/*<button className={styles.wishlistButton}>❤️</button>*/}
+                </div>
+                <div className={styles.details}>
+                    <h3 className={styles.name}>{product.name}</h3>
+                    <p className={styles.category}>Oversized Tshirt</p> {/* Add category dynamically if applicable */}
+                    <p className={styles.price}>₹ {product.price}</p>
+                </div>
             </Link>
-            <p className={styles.price}>₹ {product.price}</p>
         </div>
     );
 };
